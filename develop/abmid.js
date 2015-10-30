43f3edbe04daefbb886833dd37ffe073c73d314e
Dafrok <88609546@qq.com>
2015-10-30 12:24:28 +0800

    fixed

--- develop/abmid.js
+++ develop/abmid.js
@@ -11,9 +11,9 @@
             i,
             resultSet = [];
 
-        style.addRule(selector, "foo:bar");
+        style.addRule(selector, 'foo:bar');
         for (i = 0; i < l; i += 1) {
-            if (all[i].currentStyle.foo === "bar") {
+            if (all[i].currentStyle.foo === 'bar') {
                 resultSet.push(all[i]);
                 if (resultSet.length > maxCount) {
                     break;
@@ -34,11 +34,11 @@
     document.querySelector = document.body.querySelector = function (selector) {
         return select(selector, 1)[0] || null;
     };
-}());
+})();
 
 //Abmid
 (function Abmid(){
-    var abmids = document.querySelectorAll("[abmid]")
+    var abmids = document.querySelectorAll('[abmid]')
     var getCss = function(o,key){
         return o.currentStyle ? o.currentStyle[key] : document.defaultView.getComputedStyle(o,false)[key];
     }
@@ -46,40 +46,38 @@
         for(var abmid=0;abmid<abmids.length;abmid++){
             var m=abmids[abmid]
             var pm= m.parentElement
-            if(getCss(pm,"position")=="static" && m.parentElement.tagName!="BODY"){
-                pm.style.position="relative"
+            if(getCss(pm,'position')=='static' && m.parentElement.tagName!='BODY'){
+                pm.style.position='relative'
             }
-            m.style.position="absolute"
-            m.style.top="50%"
-            m.style.left="50%"
-            m.style.transform="translate(-50%,-50%)"
+            m.style.position='absolute'
+            m.style.top='50%'
+            m.style.left='50%'
+            m.style.transform='translate(-50%,-50%)'
         }
     }
     var setListener=function(){
         for(var abmid=0;abmid<abmids.length;abmid++){
             var m=abmids[abmid]
             var pm= m.parentElement
-            if(getCss(pm,"position")=="static" && m.parentElement.tagName!="BODY"){
-                pm.style.position="relative"
+            if(getCss(pm,'position')=='static' && m.parentElement.tagName!='BODY'){
+                pm.style.position='relative'
             }
             var innerSize=new Object(),outerSize=new Object()
             innerSize.height = m.offsetHeight
             innerSize.width = m.offsetWidth
             outerSize.height = pm.clientHeight
             outerSize.width = pm.clientWidth
-            pm.style.zoom="1"
-            m.style.position="absolute"
-            m.style.top=(outerSize.height-innerSize.height)/2+"px"
-            m.style.left=(outerSize.width-innerSize.width)/2+"px"
+            pm.style.zoom='1'
+            m.style.position='absolute'
+            m.style.top=(outerSize.height-innerSize.height)/2+'px'
+            m.style.left=(outerSize.width-innerSize.width)/2+'px'
         }
     }
-    switch(navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion .split(";")[1].replace(/[ ]/g,"")){
-        case "MSIE6.0":
-        case "MSIE7.0":
-        case "MSIE8.0":
-        case "MSIE9.0":
-            setInterval(setListener,200);break;
-        default :
-            setTransform();break;
+
+    var el = document.createElement('div');
+    if ('transform' in el.style){
+        setTransform();
+    } else {
+        setInterval(setListener,16.7);
     }
-})()
+})();
